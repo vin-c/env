275d60a71deabf76cba97bef99af7891edce5058
Vincent Gatignol-Jamon <vincent.gatignol-jamon@idgrilles.fr>
2016-09-15 11:10:57 +0200

    fix Neutron endpoint (not controller)

--- scripts/controller-setup-liberty.sh
+++ scripts/controller-setup-liberty.sh
@@ -267,9 +267,9 @@ systemctl start openstack-nova-api.service openstack-nova-cert.service \
 openstack user create --domain default --password $SERVICE_PWD neutron
 openstack role add --project service --user neutron admin
 openstack service create --name neutron --description "OpenStack Networking" network
-openstack endpoint create --region RegionOne network public http://$CONTROLLER_IP:9696
-openstack endpoint create --region RegionOne network internal http://$CONTROLLER_IP:9696
-openstack endpoint create --region RegionOne network admin http://$CONTROLLER_IP:9696
+openstack endpoint create --region RegionOne network public http://$NETWORK_IP:9696
+openstack endpoint create --region RegionOne network internal http://$NETWORK_IP:9696
+openstack endpoint create --region RegionOne network admin http://$NETWORK_IP:9696
 
 #configure nova to use neutron
 sed -i "/^\[neutron\]/a \
