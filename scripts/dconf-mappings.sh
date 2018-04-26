3100a12f7643b80116067746462be2e4121fc6da
Vincent Gatignol <vincent.gatignol@ipleanware.com>
2018-04-26 13:19:34 +0200

    wip dconf bindings

---
+++ scripts/dconf-mappings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>parenleft']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>minus']"      
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>egrave']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>underscore']" 
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>ccedilla']"       
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Super>agrave']"  
