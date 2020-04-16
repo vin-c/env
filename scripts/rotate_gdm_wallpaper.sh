387615407b507537c2f702a2197bcb61ff12286d
Vincent Gatignol <vincent.gatignol@braincube.com>
2020-04-16 17:24:52 +0200

    fix rotate script for dbus via crontab

--- scripts/rotate_gdm_wallpaper.sh
+++ scripts/rotate_gdm_wallpaper.sh
@@ -2,6 +2,10 @@
 
 local_path=~/Images/Wallpapers/laptop/divers
 
+# get pid for dbus
+pid=$(pgrep gnome-session | tail -n1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${pid}/environ|cut -d= -f2-)
+
 # get random pic
 random_pic=$(ls ${local_path} | sort -R | tail -n 1)
 
