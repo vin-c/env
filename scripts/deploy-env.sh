7945214381b8c461461a9aecb145b6b939ac8dd2
Vincent Gatignol <vincent.gatignol@braincube.com>
2022-04-10 18:58:25 +0200

    add tmux load

--- scripts/deploy-env.sh
+++ scripts/deploy-env.sh
@@ -5,20 +5,20 @@ cd $home/env
 
 git submodule init && git submodule update
 
-for file in .gitconfig .gitignore-global .gitmodules .screenrc .vimrc \
+for file in .gitconfig .gitignore-global .gitmodules .screenrc .tmux.conf .vimrc \
    .zshrc .zshrc.oh-my-zsh .oh-my-zsh ; do
-
 
    if [[ -h $home/$file ]]; then
       rm $home/$file -f
    elif [[ -e $home/$file ]]; then
       mv $home/$file $home/$file.bak
    fi
+
    ln -s $home/env/$file $home/$file
 done
 
 if [[ ! -e /bin/zsh ]]; then
-   sudo apt-get install -y zsh vim atool screen
+   sudo apt-get install -y zsh vim atool screen tmux
 fi
 
 chsh -s /bin/zsh
@@ -28,3 +28,5 @@ if [[ -e /bin/zsh ]]; then
    source $home/.zshrc
 fi
 
+echo "virtualenvwrapper script as root..."
+sudo cp scripts/virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh
