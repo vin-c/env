c49faf45bc5bd9179be374eead6401e02453dcae
Vincent Gatignol <vincent.gatignoljamon@ipsosante.fr>
2023-04-24 11:59:23 +0200

    update deploy : add ssh-agent

--- scripts/deploy-env.sh
+++ scripts/deploy-env.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 home=~
-cd $home/env
+cd ${home}/env
+
+sudo apt install -y git virtualenv virtualenvwrapper
 
 git submodule init && git submodule update
 
 for file in .gitconfig .gitignore-global .gitmodules .screenrc .tmux.conf .vimrc \
    .zshrc .zshrc.oh-my-zsh .oh-my-zsh ; do
 
-   if [[ -h $home/$file ]]; then
-      rm $home/$file -f
-   elif [[ -e $home/$file ]]; then
-      mv $home/$file $home/$file.bak
+   if [[ -h ${home}/${file} ]]; then
+      rm ${home}/${file} -f
+   elif [[ -e ${home}/${file} ]]; then
+      mv ${home}/${file} ${home}/${file}.bak
    fi
 
-   ln -s $home/env/$file $home/$file
+   ln -s ${home}/env/${file} ${home}/${file}
 done
 
 if [[ ! -e /bin/zsh ]]; then
-   sudo apt-get install -y zsh vim atool screen tmux
+   sudo apt install -y zsh vim atool screen tmux
 fi
 
 chsh -s /bin/zsh
 /bin/zsh
 
 if [[ -e /bin/zsh ]]; then
-   source $home/.zshrc
+   source ${home}/.zshrc
 fi
 
-echo "virtualenvwrapper script as root..."
-sudo cp scripts/virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh
+mkdir -p ${home}/.config/systemd/user/
+cp ${home}/env/ssh-agent.service ${home}/.config/systemd/user/ssh-agent.service
+systemctl --user daemon-reload
+systemctl --user enable ssh-agent.service
